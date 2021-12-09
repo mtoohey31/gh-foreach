6f91c4dfb2a7582a4f77f3ba1fa10924587f7004
Matthew Toohey <contact@mtoohey.com>
2021-12-08 22:12:38 -0500

    fix: remove cache properly and add tmp removal option

--- cmd/clean/clean.go
+++ cmd/clean/clean.go
@@ -1,21 +1,56 @@
 package clean
 
 import (
-	"github.com/mtoohey31/gh-foreach/helper"
+	"log"
 	"os"
+	"path"
+	"regexp"
+
+	"github.com/mtoohey31/gh-foreach/helper"
 
 	"github.com/spf13/cobra"
 )
 
 func NewCleanCmd() *cobra.Command {
+	var tmpDirs bool
+
 	cmd := &cobra.Command{
 		Use:   "clean",
 		Short: "remove cache",
 		Long:  `Delete the cached repositories.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			os.Remove(helper.GetCacheDir())
+			err := os.RemoveAll(helper.GetCacheDir())
+
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			if tmpDirs {
+				tmp, err := os.Open("/tmp")
+				if err != nil {
+					log.Fatalln(err)
+				}
+				dirnames, err := tmp.Readdirnames(0)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				tmpRe := regexp.MustCompile("^gh-foreach.*")
+				for _, item := range dirnames {
+					if tmpRe.MatchString(item) {
+						err := os.RemoveAll(path.Join("/tmp", item))
+
+						if err != nil {
+							log.Fatalln(err)
+						}
+					}
+				}
+			}
+
+			// TODO: display spaced saved
 		},
 	}
+
+	cmd.Flags().BoolVarP(&tmpDirs, "tmp-dirs", "t", false, "clean tmp dirs too")
 
 	return cmd
 }
