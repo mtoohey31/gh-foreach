685ae3d94ccec0f6c82f92629914d93bb4d43b4d
Matthew Toohey <contact@mtoohey.com>
2022-05-02 13:15:31 -0400

    build(flake): fix issues flagged by `nix flake check`

--- flake.nix
+++ flake.nix
@@ -7,21 +7,20 @@
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem
       (system:
-        with import nixpkgs { inherit system; }; rec {
-          packages.gh-foreach = buildGo118Module rec {
+        with import nixpkgs { inherit system; }; {
+          packages.default = buildGo118Module rec {
             name = "gh-foreach";
             pname = name;
             src = ./.;
             vendorSha256 = "WoiNTDnjituek7i7TSm6cN+z+cHtKeTA54IvbzUMB50=";
           };
-          defaultPackage = packages.gh-foreach;
 
-          devShell = mkShell {
+          devShells.default = mkShell {
             nativeBuildInputs = [ go_1_18 gopls ];
           };
         }) // {
-      overlay = (final: prev: {
-        gh-foreach = self.defaultPackage."${prev.system}";
+      overlays.default = (final: _: {
+        gh-foreach = self.packages."${final.system}".default;
       });
     };
 }
